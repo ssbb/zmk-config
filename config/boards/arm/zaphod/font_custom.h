2b4c5e2ffb1894e06122165a73ab4abb2fb0f148
Sviatoslav Bulbakha <mail@ssbb.me>
2023-10-07 02:58:50 +0400

    Add zaphod board and keymap

---
+++ config/boards/arm/zaphod/font_custom.h
@@ -0,0 +1,27 @@
+#include <lvgl.h>
+
+// Iosevka Bold 26 font with custom icons from FontAwesome.
+
+LV_FONT_DECLARE(font_custom_26);
+
+#define MY_SYMBOL_KEYBOARD "\xEF\x84\x9C"
+#define MY_SYMBOL_APPLE "\xEF\x85\xB9"
+#define MY_SYMBOL_LINUX "\xEF\x85\xBC"
+
+#define MY_SYMBOL_CHARGE "\xEF\x83\xA7"
+#define MY_SYMBOL_BATTERY_EMPTY "\xEE\x82\xB0"
+#define MY_SYMBOL_BATTERY_LOW "\xEE\x82\xB1"
+#define MY_SYMBOL_BATTERY_QUARTER "\xEF\x89\x83"
+#define MY_SYMBOL_BATTERY_HALF "\xEF\x89\x82"
+#define MY_SYMBOL_BATTERY_THREE_QUARTERS "\xEF\x89\x81"
+#define MY_SYMBOL_BATTERY_FULL "\xEF\x89\x80"
+#define MY_SYMBOL_USB "\xEF\x8A\x87"
+#define MY_SYMBOL_BLUETOOTH "\xEF\x8A\x93"
+#define MY_SYMBOL_GEAR "\xEF\x80\x93"
+#define MY_SYMBOL_XMARK "\xEF\x80\x8D"
+#define MY_SYMBOL_SQUARE_1 "\xEE\x89\x96"
+#define MY_SYMBOL_SQUARE_2 "\xEE\x89\x97"
+#define MY_SYMBOL_SQUARE_3 "\xEE\x89\x98"
+#define MY_SYMBOL_SQUARE_4 "\xEE\x89\x99"
+#define MY_SYMBOL_SQUARE_5 "\xEE\x89\x9A"
+#define MY_SYMBOL_SQUARE_QUESTION "\xEF\x8B\xBD"
---
+++ config/boards/arm/zaphod/widgets/layer_status.c
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+#include "../font_custom.h"
+#include "layer_status.h"
+#include <zmk/display.h>
+#include <zmk/endpoints.h>
+#include <zmk/event_manager.h>
+#include <zmk/events/layer_state_changed.h>
+#include <zmk/keymap.h>
+
+static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
+
+struct layer_status_state {
+  uint8_t index;
+  const char *label;
+};
+
+static void set_layer_symbol(lv_obj_t *label, struct layer_status_state state) {
+  if (state.label == NULL) {
+    char text[7] = {};
+
+    sprintf(text, MY_SYMBOL_KEYBOARD " %i", state.index);
+
+    lv_label_set_text(label, text);
+  } else {
+    char text[13] = {};
+    // char tmp_label[13] = {};
+    // char *system, *name, *tok_state;
+
+    // strncpy(tmp_label, state.label, sizeof(tmp_label));
+    // system = strtok_r(tmp_label, ":", &tok_state);
+    // name = strtok_r(NULL, ":", &tok_state);
+
+    // if (strcmp(system, "MAC") == 0) {
+    //     snprintf(text, sizeof(text), MY_SYMBOL_APPLE " %s", name);
+    // } else if (strcmp(system, "NIX") == 0) {
+    //     snprintf(text, sizeof(text), MY_SYMBOL_LINUX " %s", name);
+    // } else {
+    //     snprintf(text, sizeof(text), MY_SYMBOL_KEYBOARD " %s", state.label);
+    // }
+
+    snprintf(text, sizeof(text), MY_SYMBOL_KEYBOARD " %s", state.label);
+
+    lv_label_set_text(label, text);
+  }
+}
+
+static void layer_status_update_cb(struct layer_status_state state) {
+  struct zmk_widget_layer_status *widget;
+  SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
+    set_layer_symbol(widget->obj, state);
+  }
+}
+
+static struct layer_status_state layer_status_get_state(const zmk_event_t *eh) {
+  uint8_t index = zmk_keymap_highest_layer_active();
+  return (struct layer_status_state){.index = index,
+                                     .label = zmk_keymap_layer_label(index)};
+}
+
+ZMK_DISPLAY_WIDGET_LISTENER(widget_layer_status, struct layer_status_state,
+                            layer_status_update_cb, layer_status_get_state)
+
+ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);
+
+int zmk_widget_layer_status_init(struct zmk_widget_layer_status *widget,
+                                 lv_obj_t *parent) {
+  widget->obj = lv_label_create(parent);
+  sys_slist_append(&widgets, &widget->node);
+  widget_layer_status_init();
+
+  return 0;
+}
+
+lv_obj_t *zmk_widget_layer_status_obj(struct zmk_widget_layer_status *widget) {
+  return widget->obj;
+}
